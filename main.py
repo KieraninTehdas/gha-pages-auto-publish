b01ab0456e8903e5208e9c818ea83c806fb8cd5a
Kieran Porter <k.porter.phys@gmail.com>
2020-05-24 17:26:05 +0100

    Finding file which need to be updated

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from pprint import pprint
+import os
+import glob
 import datetime
 import json
 
 TRIGGER_PAYLOAD_PATH = '/github/workflow/event.json'
+WORKSPACE_PATH = '/github/workspace/'
 
 
 def main() -> None:
@@ -13,18 +15,40 @@ def main() -> None:
         input_event = json.load(f)
 
     schedule = input_event['schedule']
+    # TODO: could have an optional input that specifies last run time? Useful for on-demand?
+
+    # TODO: list _posts content. Take first 10 chars to get date. Parse and find between last run and current run
 
 
-def parse_cron(cron_expression: str) -> datetime.timedelta:
+def strip_cron_expression(cron_expression: str) -> int:
+    return int(cron_expression.replace('*', '').replace('/', ''))
+
+
+def get_last_run_date(cron_expression: str) -> datetime.date:
+    """Work out the last action run date based on the cron schedule.
+        Assumes that the schedule never changes, which isn't great.
+        For now only works with a set time every n days.
+    """
+
     expressions = cron_expression.split(' ')
 
     if len(expressions) != 5:
         raise ValueError(
-            f'Unexpected Cron Expression Length. Expected 6: Found {len(expressions)}')
+            f'Unexpected Cron Expression Length. Expected 5: Found {len(expressions)}')
 
-    datetime.timedelta(m)
+    every_n_days = datetime.timedelta(
+        days=strip_cron_expression(expressions[2]))
+
+    return datetime.date.today() - every_n_days
+
+
+def construct_glob_params(today_date: datetime.date, last_run_date: datetime.date) -> str:
+    td = today_date - last_run_date
+
+    return td
 
 
 if __name__ == "__main__":
-    ['minutes', 'hours']
-    pprint(datetime.timedelta(**{'minutes': 1}))
+    lrd = get_last_run_date("0 12 */3 * *")
+
+    print(construct_glob_params(datetime.date.today(), lrd))
